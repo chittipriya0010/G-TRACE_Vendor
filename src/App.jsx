95994de8f80c34204744825a9cccd68c3f4f4749
UjjwalFullStackDev <ujjwal.tyagi90611@gmail.com>
2025-09-12 18:51:16 +0530

    Added header and sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import { Routes, Route } from "react-router-dom";
-// import VendorManagementSystem from "./pages/VendorManagementSystem/VendorManagementSystem";
-import VendorManagementSystem from "./pages/VendorManagementSystem";
+import VendorManagementSystem from "./pages/VendorManagementSystem/VendorManagementSystem";
 import VendorRoutes from "./pages/VendorManagementSystem/VendorRoutes";
 
 function App() {
   return (
     <Routes>
       {/* Layout wrapper */}
-      <Route path="/" element={<VendorManagementSystem />}>
-      {/* <Route path="/" element={<VendorManagementSystem />}> */}
-      {/* <Route path="/vendors/*" element={<VendorManagementSystem />}> */}
-
+      <Route path="/vendors" element={<VendorManagementSystem />}>
+        {/* All nested routes go inside */}
+        <Route path="*" element={<VendorRoutes />} />
       </Route>
     </Routes>
   );
