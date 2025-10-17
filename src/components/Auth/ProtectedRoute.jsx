9e6fc152a2759bf9b5e8f0b24e51ad3ca91d7236
Chittipriya <chitti1890@gmail.com>
2025-10-17 15:18:49 +0530

    Gtrac Dashboard

--- src/components/Auth/ProtectedRoute.jsx
+++ src/components/Auth/ProtectedRoute.jsx
@@ -10,22 +10,22 @@ const ProtectedRoute = ({ allowedRoles = [], allowedPermissions = [], children }
 
   const user = JSON.parse(userData);
   const userRole = user.role;
-  const userPermissions = user.permissions || [];
+  // const userPermissions = user.permissions || [];
 
   console.log("✅ ProtectedRoute Debug");
   console.log("Allowed Roles:", allowedRoles);
   console.log("User Role:", userRole);
-  console.log("User Permissions:", userPermissions);
+  // console.log("User Permissions:", userPermissions);
 
   const hasRole =
     allowedRoles.length === 0 ||
     allowedRoles.some(role => normalize(role) === normalize(userRole));
 
-  const hasPermission =
-    allowedPermissions.length === 0 ||
-    allowedPermissions.some(perm => userPermissions.includes(perm));
+  // const hasPermission =
+  //   allowedPermissions.length === 0 ||
+  //   allowedPermissions.some(perm => userPermissions.includes(perm));
 
-  if (!hasRole || !hasPermission) {
+  if (!hasRole) {
     console.warn("🚫 User is not authorized to access this page!");
     return <Navigate to="/unauthorized" replace />;
   }
